be5cdb39a3ae503ab84ec875f0d444570adb0d61
Ankush kumar <ankushkumar1412005@gmail.com>
2025-06-01 22:15:25 +0530

    update size

--- src/pages/UserDashboard/Orders.jsx
+++ src/pages/UserDashboard/Orders.jsx
@@ -37,16 +37,16 @@ function Orders() {
   };
 
   return (
-    <div className="p-6 mx-auto overflow-x-hidden">
-      <h2 className="text-3xl font-bold text-indigo-700 mb-8 text-center">Your Orders</h2>
+    <div className="p-6 overflow-x-hidden">
+      <h2 className="md:text-3xl text-2xl font-bold text-indigo-700 mb-8 text-center">Your Orders</h2>
 
       {orders.length === 0 ? (
         <p className="text-center text-gray-500">You haven't placed any orders yet.</p>
       ) : (
         orders.map((order) => (
           <div key={order._id} className="bg-white border flex-col justify-center items-center border-gray-200 rounded-xl p-6 mb-6 shadow-md hover:shadow-lg transition">
-            <div className="flex justify-between items-center mb-2">
-              <h3 className="text-lg font-semibold text-indigo-600">Order ID: <span className="text-black">{order._id}</span></h3>
+            <div className="flex flex-wrap justify-between items-center mb-2">
+              <h3 className="md:text-lg text-[14px] font-semibold text-indigo-600">Order ID: <span className="text-black">{order._id}</span></h3>
               {getStatusBadge(order.status)}
             </div>
 
@@ -57,7 +57,7 @@ function Orders() {
               <h4 className="font-semibold mb-2 text-gray-800">Items in this order:</h4>
               <ul className="space-y-4">
                 {order.items.map((item, index) => (
-                  <li key={index} className="flex items-center space-x-4">
+                  <li key={index} className="flex flex-wrap items-center space-x-4">
                     <img
                       src={item.photo}
                       alt={item.name}
